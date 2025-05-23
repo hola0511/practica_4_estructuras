ae39f359d58225d14428d1209afdbc4d2cd91a1c
samuel espinosa arias <76191247+hola0511@users.noreply.github.com>
2025-05-23 08:30:07 -0500

    Actualizar person.py

--- codigo/person.py
+++ codigo/person.py
@@ -18,26 +18,33 @@ class Person:
         self.disabled_dirs = []
         self.delay = False
 
-    def take_turn(self, grid, exit_pos, iteration):
-        if self.delay:
-            print(f"Persona en {self.pos} pierde turno.")
-            self.delay = False
-            return
+    def take_turn(self, grid, exits, iteration):
+    if self.delay:
+        print(f"Persona en {self.pos} pierde turno.")
+        self.delay = False
+        return
 
-        path = calculate_path(grid, self.pos, exit_pos, self.disabled_dirs)
-        if not path or len(path) < 2:
-            print(f"ay muchachos... (sin salida desde {self.pos})")
-            return
+    path = None
+    for salida in exits:
+        temp_path = calculate_path(grid, self.pos, salida, self.disabled_dirs)
+        if temp_path and (not path or len(temp_path) < len(path)):
+            path = temp_path
 
-        next_pos = path[1]
-        direction = (next_pos[0] - self.pos[0], next_pos[1] - self.pos[1])
-        self.current_node = self._add_decision(next_pos, iteration, direction)
-        self.pos = next_pos
-        cell = grid[self.pos[0]][self.pos[1]]
-        if cell == "T":
-            self.disabled_dirs.append(choice(DIRECTIONS))
-        elif cell == "R":
-            self.delay = True
+    if not path or len(path) < 2:
+        print(f"ay muchachos... (sin salida desde {self.pos})")
+        return
+
+    next_pos = path[1]
+    direction = (next_pos[0] - self.pos[0], next_pos[1] - self.pos[1])
+    self.current_node = self._add_decision(next_pos, iteration, direction)
+    self.pos = next_pos
+
+    cell = grid[self.pos[0]][self.pos[1]]
+    if cell == "T":
+        from random import choice
+        self.disabled_dirs.append(choice([(-1,0),(1,0),(0,-1),(0,1), (-1,-1),(-1,1),(1,-1),(1,1)]))
+    elif cell == "R":
+        self.delay = True
 
     def _add_decision(self, pos, iteration, direction):
         node = DecisionNode(pos, iteration, direction)
